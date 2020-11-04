03826ee8f29325784fc0d97896921e8ebd843aa9
Pilyukov Maxim <filiniti@gmail.com>
2020-11-04 18:45:02 +0500

    1 0 8

---
+++ BaseBotLib/Services/Logger/ConsoleLogger.cs
@@ -0,0 +1,31 @@
+﻿using BaseBotLib.Interfaces.Logger;
+using System;
+
+namespace BaseBotLib.Services.Logger
+{
+    public class ConsoleLogger : ILogger
+    {
+        private string CurrentTime()
+        {
+            return DateTime.UtcNow.ToString("HH:mm");
+        }
+
+        public void Info(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Info ({CurrentTime()}) : {text}");
+        }
+
+        public void Warn(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Warn ({CurrentTime()}): {text}");
+        }
+
+        public void Error(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error ({CurrentTime()}): {text}");
+        }
+    }
+}
---
+++ BaseBotLib/Services/Logger/FileConsoleLogger.cs
@@ -0,0 +1,71 @@
+﻿using BaseBotLib.Interfaces.Logger;
+using System;
+using System.IO;
+
+namespace BaseBotLib.Services.Logger
+{
+    public class FileConsoleLogger : ILogger
+    {
+        private readonly string _fileFolder;
+        private readonly ILogger _defaultlogger;
+
+        public FileConsoleLogger(string fileFolder, ILogger defaultlogger = null)
+        {
+            _fileFolder = fileFolder;
+            _defaultlogger = defaultlogger ?? new ConsoleLogger();
+        }
+
+        private string CurrentTime()
+        {
+            return DateTime.UtcNow.ToString("HH:mm");
+        }
+
+        private void Write(string data)
+        {
+            try
+            {
+                if (!Directory.Exists(_fileFolder))
+                {
+                    Directory.CreateDirectory(_fileFolder);
+                }
+
+                var fileName = $"{_fileFolder}/logs_{DateTime.UtcNow.Date:dd.MM.yyyy}.txt";
+
+                File.AppendAllText(fileName, data + Environment.NewLine);
+            }
+            catch (Exception e)
+            {
+                _defaultlogger.Warn($"Ошибка при записи логов в файл : {e}.");
+            }
+        }
+
+        public void Info(string text)
+        {
+            var content = $"Info ({CurrentTime()}) : {text}";
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(content);
+            Write(content);
+        }
+
+        public void Warn(string text)
+        {
+            var content = $"Warn ({CurrentTime()}) : {text}";
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(content);
+
+            Write(content);
+        }
+
+        public void Error(string text)
+        {
+            var content = $"Error ({CurrentTime()}) : {text}";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(content);
+
+            Write(content);
+        }
+    }
+}
